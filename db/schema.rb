e3171b2fcc69490d06d1990b92fe0ed0fc09547b
AliAlsayed <a_alsayed87@hotmail.com>
2020-04-03 20:35:42 +0400

    generate devise views and add attributes to devise user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_163815) do
+ActiveRecord::Schema.define(version: 2020_04_03_142638) do
 
   create_table "offers", force: :cascade do |t|
     t.integer "buyers_required"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 2020_04_01_163815) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_seller", default: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
